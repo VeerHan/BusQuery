583f2f00439e7f95b30498bbc5946d36f4712f6f
Leo.Han <leoleohan@foxmail.com>
2016-04-14 20:39:36 +0800

    add fragment

--- app/src/main/java/com/john/busquery/activity/MainActivity.java
+++ app/src/main/java/com/john/busquery/activity/MainActivity.java
@@ -2,11 +2,16 @@ package com.john.busquery.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.john.busquery.R;
+import com.john.busquery.adapter.MyFragmentPagerAdapter;
+import com.john.busquery.fragment.LineFragment;
+import com.john.busquery.fragment.StationFragment;
+import com.john.busquery.fragment.SwitchFragment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,19 +25,23 @@ public class MainActivity extends AppCompatActivity {
     ViewPager viewPager;
     @InjectView(R.id.tabLayout)
     TabLayout tabLayout;
-
-    //页卡视图
-    private View view1, view2, view3;
     //页卡视图标题
-    private String[] titles = {"a", "b", "c"};
+    private String[] titles = {"线路", "换乘", "站点"};
     //页卡视图集合
-    private List<View> viewList = new ArrayList<>();
+    private List<Fragment> fragmentList = new ArrayList<>();
+    private MyFragmentPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
-
+        fragmentList.add(new LineFragment());
+        fragmentList.add(new SwitchFragment());
+        fragmentList.add(new StationFragment());
+        adapter = new MyFragmentPagerAdapter(getSupportFragmentManager(), titles, fragmentList);
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
+
 }
---
+++ app/src/main/java/com/john/busquery/adapter/MyFragmentPagerAdapter.java
@@ -0,0 +1,35 @@
+package com.john.busquery.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    private String[] titles;
+    private List<Fragment> fragmentList;
+
+    public MyFragmentPagerAdapter(FragmentManager fm, String[] titles, List<Fragment> fragmentList) {
+        super(fm);
+        this.titles = titles;
+        this.fragmentList = fragmentList;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles[position];
+    }
+}
